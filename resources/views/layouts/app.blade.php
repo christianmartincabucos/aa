c22bac8ac5bbc62ad2cda5d6ea8b1166b08bd810
christian martin cabucos <christianmartincabucos@gmail.com>
2020-10-15 15:48:58 +0800

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,18 @@
     <style>
         @font-face {
             font-family: "Gotham";
-            src: '../fonts/Gotham/gotham.woff2'
+            src: url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.eot");
+            /* IE9*/
+            src: url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.eot?#iefix") format("embedded-opentype"),
+                /* IE6-IE8 */
+                url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.woff2") format("woff2"),
+                /* chrome firefox */
+                url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.woff") format("woff"),
+                /* chrome firefox */
+                url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.ttf") format("truetype"),
+                /* chrome firefox opera Safari, Android, iOS 4.2+*/
+                url("https://db.onlinewebfonts.com/t/edc378fa3251080c5b508b220462efca.svg#Gotham") format("svg");
+            /* iOS 4.1- */
         }
 
         * {
