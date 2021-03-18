7036d35fbbbf19e798b69d283fb67bb2d02efc1c
christian martin cabucos <35110272+christianmartincabucos@users.noreply.github.com>
2021-03-18 16:40:40 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1683,6 +1683,29 @@
         <!-- Copyright -->
 
     </footer>
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="101481898595524">
+      </div>
     <script src="{{ secure_asset('js/app.js') }}"></script>
     <script src="{{ secure_asset('js/jquery.simplePagination.js') }}"></script>
     <!-- <script>
